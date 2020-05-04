f3208fb9aa7fcc8b0f37164e5bd74ae649f6b7c8
Ashley Allphin <ashley.allphin@gmail.com>
2020-05-04 18:50:05 -0400

    removing unnecessary  packages

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
 
-app.use(methodOverride('_method'));
 app.engine('handlebars', exphhbs({
     //main.handlebars
     defaultLayout: 'main'
