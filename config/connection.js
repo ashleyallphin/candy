19cd8c97a72d31da49b4b343bcdb46a1abf9cf50
Ashley Allphin <ashley.allphin@gmail.com>
2020-05-05 20:19:35 -0400

    for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,23 @@
-//connect Node to MySQL
-
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'wallpaper',
-    database: 'candy_db',
+    host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'buo30rvf6z6zwa4o',
+    password: 'm8irp59q4916vd8u',
+    database: 'bygbjx7n0gi8mkkg',
 })
 
-// var mysql = require("mysql");
+var mysql = require("mysql");
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'wallpaper',
-//         database: 'candy_db',
-//     });
-// };
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'wallpaper',
+        database: 'candy_db',
+    });
+};
 
 connection.connect(function(err){
     if(err)throw err;
